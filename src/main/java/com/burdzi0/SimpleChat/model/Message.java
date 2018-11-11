50a576897ef985e864f9ea20bb0344aadc703e16
Burdzi0 <burdzi0@programmer.net>
2018-11-12 00:02:45 +0100

    Add lombok annotations to Message

--- src/main/java/com/burdzi0/SimpleChat/model/Message.java
+++ src/main/java/com/burdzi0/SimpleChat/model/Message.java
@@ -1,10 +1,15 @@
 package com.burdzi0.SimpleChat.model;
 
 import com.burdzi0.SimpleChat.listener.Slf4jMessageLogListener;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.*;
 import java.util.Date;
 
+@Data
+@NoArgsConstructor
 @Entity
 @EntityListeners(Slf4jMessageLogListener.class)
 public class Message {
@@ -24,66 +29,5 @@ public class Message {
 		this.author = author;
 		this.content = content;
 		this.sendingTime = sendingTime;
-	}
-
-	public Message() {
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Author getAuthor() {
-		return author;
-	}
-
-	public void setAuthor(Author author) {
-		this.author = author;
-	}
-
-	public String getContent() {
-		return content;
-	}
-
-	public void setContent(String content) {
-		this.content = content;
-	}
-
-	public Date getSendingTime() {
-		return sendingTime;
-	}
-
-	public void setSendingTime(Date sendingTime) {
-		this.sendingTime = sendingTime;
-	}
-
-
-	@Override
-	public boolean equals(Object obj) {
-		if (obj == null || obj.getClass() != this.getClass()) return false;
-		Message objDate = (Message) obj;
-		if (!objDate.getId().equals(id)) return false;
-		if (!objDate.getSendingTime().equals(sendingTime)) return false;
-		if (!objDate.author.equals(author)) return false;
-		return objDate.getContent().equals(content);
-	}
-
-	@Override
-	public int hashCode() {
-		return id.hashCode() * author.hashCode() * content.hashCode() * sendingTime.hashCode();
-	}
-
-	@Override
-	public String toString() {
-		return "Message{" +
-				"id=" + id +
-				", author=" + author.getUsername() +
-				", content='" + content + '\'' +
-				", sendingTime=" + sendingTime +
-				'}';
 	}
 }
